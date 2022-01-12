8c0139bc90c8dbf481de62a1843617e9db6094c8
slimer37 <59073967+slimer37@users.noreply.github.com>
2022-01-11 21:11:48 -0800

    Minor correction

--- ReconFileGenerator/Cube.cs
+++ ReconFileGenerator/Cube.cs
@@ -49,7 +49,7 @@ public static class Cube
         "Small Lightning Bolt",
         "Square Shape",
         "T Shape",
-        "W Shap"
+        "W Shape"
     };
 
     public static readonly string[] PllPerms =
